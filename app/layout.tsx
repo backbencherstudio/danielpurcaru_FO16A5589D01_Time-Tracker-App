166a5fa9af4577fea26c6a79527fbd6f92aa6bcb
Miraz Hossain <105309402+Miraz661@users.noreply.github.com>
2025-06-28 17:02:32 +0600

    Navbar and sidebar added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,20 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { AppConfig } from "@/config/app.config";
+import { Urbanist } from 'next/font/google';
+import Navbar from "@/components/Shared/Navbar";
+import Sidebar from "@/components/Shared/Sidebar";
 
 export const metadata: Metadata = {
   title: AppConfig().app.name,
   description: AppConfig().app.slogan,
 };
+
+const urbanist = Urbanist({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'], // Add any weights you need
+  display: 'swap',
+});
 
 export default function RootLayout({
   children,
@@ -13,8 +22,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body>{children}</body>
+    <html lang="en" suppressHydrationWarning className={urbanist.className}>
+      <body className="bg-white flex justify-center">
+        <div className="h-screen w-screen overflow-hidden bg-white max-w-[1440px]">
+          <Navbar />
+          <div className="w-full flex bg-white">
+            <Sidebar />
+            <div className="bg-[#F8FAFB] h-full w-full px-5 py-6">
+              {children}
+            </div>
+          </div>
+        </div>
+      </body>
     </html>
   );
 }
