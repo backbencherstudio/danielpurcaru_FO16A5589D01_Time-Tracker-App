b6b7ba9b86fe2680dd199f39fd497af23772d2bc
hasanakhonda <mdmahmudhasanakhonda@gmail.com>
2025-06-30 15:44:52 +0600

    callender added

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,16 @@
+import LoginPage from "../loginPage/LoginPage";
+import Image from "next/image";
+import RegisterPage from "../registerpage/RegisterPage";
+import RecoveryPage from "../recoveryPage/RecoveryPage";
+import OTPPage from "../otp/OTPPage";
+import CreateNewPasswordPage from "../createnewpasswordpage/CreateNewPasswordPage";
+
+
+export default function Login() {
+  return (
+    <div >
+      <LoginPage/>
+      
+    </div>
+  );
+}
